eed82e5f0349359037ebb1bf49129a547e9ba89b
Mister I want to  know everything <166235967+hytuka170596@users.noreply.github.com>
2024-11-24 21:11:48 +0400

    Update forms.py

--- mysite/shopapp/forms.py
+++ mysite/shopapp/forms.py
@@ -85,43 +85,19 @@ class MultipleImageInput(forms.ClearableFileInput):
 
 
 class MultipleImageField(forms.FileField):
-    """
-    Custom form field for handling multiple image uploads.
-
-    Args:
-        *args: Positional arguments for the parent class.
-        **kwargs: Keyword arguments for the parent class.
-
-    Methods:
-        clean(images: Union[List[files], files], initial=None) -> Union[List[files], files]:
-            Cleans and validates uploaded images.
-    """
 
     def __init__(self, *args, **kwargs) -> None:
         kwargs.setdefault("widget", MultipleImageInput())
         super().__init__(*args, **kwargs)
 
-    def clean(
-        self, images: Union[List[ImageFile], ImageFile], initial=None
-    ) -> Union[List[ImageFile], ImageFile]:
-        """
-        Clean and validate uploaded images.
-
-        Args:
-            images (Union[List[ImageFile], ImageFile]): The uploaded images to clean.
-            initial: Initial data for cleaning.
-
-        Returns:
-            Union[List[ImageFile], ImageFile]: A list of cleaned images or a single cleaned image.
-        """
-
+    def clean(self, images, initial=None):
         single_file_clean = super().clean
         if isinstance(images, (list, tuple)):
-            result: List[ImageFile] = [
+            result = [
                 single_file_clean(image, initial) for image in images
             ]
         else:
-            result: ImageFile = [single_file_clean(images, initial)]
+            result = [single_file_clean(images, initial)]
 
         return result
 
