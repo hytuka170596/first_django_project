0d561fc1d847c470d4d4c9159a151c4c70fc2c1a
Дмитрий Афанасьев <wackymc@yandex.ru>
2024-11-25 16:16:44 +0400

    Added static_root in settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -200,6 +200,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "uploads"
