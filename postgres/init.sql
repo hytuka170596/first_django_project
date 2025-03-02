110636f5cddaac64764425db28c4dffeccd3c3c9
Mister I want to  know everything <Wackymc@yandex.ru>
2025-03-02 18:08:57 +0400

    Update init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,3 +1,3 @@
-CREATE USER admin WITH PASSWORD 'lilden783';
+CREATE USER admin WITH PASSWORD 'password';
 CREATE DATABASE postgres_db OWNER admin;
 GRANT ALL PRIVILEGES ON DATABASE postgres_db TO admin;
